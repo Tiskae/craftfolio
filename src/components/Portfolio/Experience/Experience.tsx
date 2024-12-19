02939abb8fa01c349c212ec60fb76c798e6ab6a6
Tiskae <adedokuntobiloba100@gmail.com>
2024-12-19 12:45:16 +0100

    feat: education section

--- src/components/Portfolio/Experience/Experience.tsx
+++ src/components/Portfolio/Experience/Experience.tsx
@@ -49,21 +49,21 @@ const Experience: React.FC = () => {
       employmentType: "Part-time",
       skills: ["JavaScript", "CSS", "React.js", "Redux.js", "Node.js", "SASS", "HTML", "Websockets", "Search Engine Optimization (SEO)",],
     },
-    {
-      company: "FitSit",
-      jobtitle: "Frontend Web Developer",
-      startDate: new Date(),
-      endDate: new Date(),
-      description: `- Led frontend development using React and SASS, implementing authentication, security, and state management with Redux. Improved user experience by overhauling >90% of the UI/UX codebase for reliability and cross-browser consistency.
+//     {
+//       company: "FitSit",
+//       jobtitle: "Frontend Web Developer",
+//       startDate: new Date(),
+//       endDate: new Date(),
+//       description: `- Led frontend development using React and SASS, implementing authentication, security, and state management with Redux. Improved user experience by overhauling >90% of the UI/UX codebase for reliability and cross-browser consistency.
 
-                    - Coordinated effectively with backend, cloud, and design teams within an AGILE methodology while working fully remote. Successfully added translations using i18next to support 4 languages, handled error and socket connection.
+//                     - Coordinated effectively with backend, cloud, and design teams within an AGILE methodology while working fully remote. Successfully added translations using i18next to support 4 languages, handled error and socket connection.
 
-                    - Spearheaded enhancements to the frontend codebase at FitSit as a Lead Developer, tuning every other aspect of frontend development to improve the speed and overall functionality of the website.
-`,
-      location: { place: "Prague, Czech", type: "Remote" },
-      employmentType: "Part-time",
-      skills: ["JavaScript", "CSS", "React.js", "Redux.js", "Node.js", "SASS", "HTML", "Websockets", "Search Engine Optimization (SEO)",],
-    },
+//                     - Spearheaded enhancements to the frontend codebase at FitSit as a Lead Developer, tuning every other aspect of frontend development to improve the speed and overall functionality of the website.
+// `,
+//       location: { place: "Prague, Czech", type: "Remote" },
+//       employmentType: "Part-time",
+//       skills: ["JavaScript", "CSS", "React.js", "Redux.js", "Node.js", "SASS", "HTML", "Websockets", "Search Engine Optimization (SEO)",],
+//     },
   ];
 
   return (
